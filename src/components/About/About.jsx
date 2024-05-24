a2a05cd2340a754077e09ca41f72042f602cd433
anikmondol <anikmondol558363@gmil.com>
2024-05-24 11:50:31 +0600

    rest-countries-react projects setup done

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <div>
+            <h2>I am from About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit, labore.</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>I am from Footer</small></p>
+        </div>
+    );
+};
+
+export default Footer;
